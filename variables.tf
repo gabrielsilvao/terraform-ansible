9f9f1e66c943145b9b51e93ea04eb64e2330e7b9
loktwo <loktwo@outlook.com>
2021-06-21 22:13:34 -0300

    codes v2

--- variables.tf
+++ variables.tf
@@ -1,9 +1,34 @@
+variable "name" {
+    type = string
+    default = default
+}
+
+variable "region-master" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "region-worker" {
+    type = string
+    default = "us-east-2"
+}
+
+variable "external_ip" {
+    type = string
+    default = "0.0.0.0/0"
+}
+
 variable "workers-count" {
-  type    = number
-  default = 1
+    type    = number
+    default = 1
 }
 
 variable "instance-type" {
-  type    = string
-  default = "t3.micro"
+    type    = string
+    default = "t3.micro"
+}
+
+variable "webserver-port" {
+    type = number
+    default = 80
 }
